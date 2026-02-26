d20fac28ed6322d5d68f7b2f12ef099e5ff35b21
Benita Almroth <benita.almroth@hotmail.com>
2026-02-26 11:45:28 +0100

    added products view, product controller and index() in dashboardcontroller

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,4 +2,18 @@
 
 <p>Hello, {{ $user->name }}!</p>
 
+<h2>Options</h2>
+<div class="grid">
+    <a href="{{ route('products.index') }}" class="card">Show all products</a>
+    <a href="{{ route('products.create') }}" class="card">Add Product</a>
+</div>
+
+<h2>Choose Brand</h2>
+<div class="grid">
+    @foreach ($brands as $brand)
+        <a href="{{ route('products.byBrand', $brand->id) }}" class="card">{{ $brand->name }}</a>
+    @endforeach
+</div>
+
+
 <a href="/logout"> logout </a>
