38c32e93784bb2d6fe68b50442157bec559d84cd
Benita Almroth <benita.almroth@hotmail.com>
2026-03-06 09:00:56 +0100

    made description nullable. removed nullable on prize

--- database/migrations/2026_02_25_125605_create_products_table.php
+++ database/migrations/2026_02_25_125605_create_products_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->foreignId('brand_id')->constrained('brands');
             $table->foreignId('condition_id')->constrained('conditions');
-            $table->integer('price')->nullable();
+            $table->integer('price');
             $table->timestamps();
         });
     }
