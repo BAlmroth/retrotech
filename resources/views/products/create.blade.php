f4dfc9eac099d3ce189e0669cf3ebd3cb2c61c81
Benita Almroth <benita.almroth@hotmail.com>
2026-03-09 12:40:08 +0100

    added aria-describedby in create.blade.php for a11y

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -10,34 +10,34 @@
             
             <div>
                 <label for="name">Product name</label>
-                <input type="text" name="name" id="name" value="{{ old('name') }}">
-                @error('name') <span class="field-error">{{ $message }}</span> @enderror
+                <input type="text" name="name" id="name" value="{{ old('name') }}" aria-describedby="name-error">
+                @error('name') <span class="field-error" id="name-error">{{ $message }}</span> @enderror
             </div>
             
             <div>
                 <label for="brand_id">Brand</label>
-                <select name="brand_id" id="brand_id">
+                <select name="brand_id" id="brand_id" aria-describedby="brand_id-error">
                     @foreach($brands as $brand)
                     <option value="{{ $brand->id }}" {{ old('brand_id') == $brand->id ? 'selected' : '' }}>{{ $brand->name }}</option>
                     @endforeach
                 </select>
-                @error('brand_id') <span class="field-error">{{ $message }}</span> @enderror
+                @error('brand_id') <span class="field-error" id="brand_id-error">{{ $message }}</span> @enderror
             </div>
             
             <div>
                 <label for="condition_id">Condition</label>
-                <select name="condition_id" id="condition_id">
+                <select name="condition_id" id="condition_id" aria-describedby="condition_id-error">
                     @foreach($conditions as $condition)
                     <option value="{{ $condition->id }}" {{ old('condition_id') == $condition->id ? 'selected' : '' }}>{{ $condition->name }}</option>
                     @endforeach
                 </select>
-                @error('condition_id') <span class="field-error">{{ $message }}</span> @enderror
+                @error('condition_id') <span class="field-error" id="condition_id-error">{{ $message }}</span> @enderror
             </div>
             
             <div>
                 <label for="price">Price (kr)</label>
-                <input type="number" name="price" id="price" step="0.01" value="{{ old('price') }}">
-                @error('price') <span class="field-error">{{ $message }}</span> @enderror
+                <input type="number" name="price" id="price" step="0.01" value="{{ old('price') }}" aria-describedby="price-error">
+                @error('price') <span class="field-error" id="price-error">{{ $message }}</span> @enderror
             </div>
             
             <div>
