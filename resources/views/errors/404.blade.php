9eeba966c0694b4aa17a005953829163ce4921bc
Benita Almroth <benita.almroth@hotmail.com>
2026-03-12 13:00:25 +0100

    aligned error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Error: 404</h1>
-<p>Page does not exist</p>
-<a href="{{url()->previous() }}">Go to previous page</a>
+<div class="errorPage">
+    <h1>Error: 404</h1>
+    <p>Page does not exist</p>
+    <a href="{{url()->previous() }}">Go to previous page</a>
+</div>
     
 @endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Error: 500</h1>
-<p>Something went wrong, please try again or reach out to support:</p>
-<p>support@retrotech.se</p>
-<a href="{{url()->previous() }}">Go to previous page</a>
+<div class="errorPage">
+    <h1>Error: 500</h1>
+    <p>Something went wrong, please try again or reach out to support:</p>
+    <p>support@retrotech.se</p>
+    <a href="{{url()->previous() }}">Go to previous page</a>
+</div>
     
 @endsection
